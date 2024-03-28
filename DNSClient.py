d714cb8882e3f51ddff7ff4aaa295acdc07bc8d8
mn3585 <158656925+mn3585@users.noreply.github.com>
2024-03-28 16:09:25 -0400

    DNSClient.py

--- DNSClient.py
+++ DNSClient.py
@@ -44,7 +44,7 @@ def local_external_DNS_output(question_type):
         print(f"The IP address of {domain_name} is {ip_address}")
 
 
-    print("\nPublic DNS Server")
+    #print("\nPublic DNS Server")
 
     for domain_name in domainList:
         ip_address = query_dns_server(domain_name,question_type)
